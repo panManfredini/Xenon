9910c6c8183bb1f1cb79eaa12dcc208d0c007f5f
alessandro manfredini <manfredini@xe-grid00.lngs.infn.it>
2014-11-04 14:08:49 +0100

    restored RunRate

--- Run_12_rate_Problem/rate.C
+++ Run_12_rate_Problem/rate.C
@@ -18,17 +18,15 @@ void rate::Loop()
    if(XeT1.fChain ==0) return;
 
   
-   Initialize();
-
+//------------------------HISTOS---------------------------------------------//
    TH1F *cut_flow = new TH1F("cut_flow","",50, -0.5, 49.5);
 	cut_flow->GetXaxis()->SetBinLabel(1,"Tot");
 	cut_flow->GetXaxis()->SetBinLabel(2,"bruno_basic");
 	cut_flow->GetXaxis()->SetBinLabel(3,"X48kg0");
 	cut_flow->GetXaxis()->SetBinLabel(4,"S1Tot_3_20");
-	cut_flow->GetXaxis()->SetBinLabel(5,"Xs2peakpos0");
-	cut_flow->GetXaxis()->SetBinLabel(6,"X34kg2");
-	cut_flow->GetXaxis()->SetBinLabel(7,"Livetime");
-       cut_flow->Sumw2();
+	cut_flow->GetXaxis()->SetBinLabel(5,"X34kg2");
+	cut_flow->GetXaxis()->SetBinLabel(6,"Livetime");
+        cut_flow->Sumw2();
 
 
    TH1F *rate_r = new TH1F("rate_r","rate in volume integrating in Z",1000, 0., 200.);
@@ -47,11 +45,63 @@ void rate::Loop()
 
    rate_r->Sumw2();
    rate_z->Sumw2();
-   
+
+  // unixtime from 2008 to 2017 in bin of 10min
+    TH1F *rate_bruno = new TH1F("rate_bruno","rate VS dataset time",500000,1200000000.,1500000000.); 
+    TH1F *rate_48kg = new TH1F("rate_48kg","rate VS dataset time",500000,1200000000.,1500000000.);
+    TH1F *rate_SR = new TH1F("rate_SR","rate VS dataset time",500000,1200000000.,1500000000.); 
+    TH1F *h_livetime = new TH1F("h_livetime","livetime VS dataset time",500000,1200000000.,1500000000.); 
+    TH1F *time_start = new TH1F("time_start","start time of dataset",500000,1200000000.,1500000000.);
+    TH1F *time_end = new TH1F("time_end","start time of dataset",500000,1200000000.,1500000000.);
+
+    TH1F *event_time_bruno = new TH1F("event_time_bruno","Events per 10 min",144,0.,86400.);
+    TH1F *event_time_48kg = new TH1F("event_time_48kg","Events per 10 min",144,0.,86400.);
+    TH1F *event_time_SR = new TH1F("event_time_SR","Events per 10 min",144,0.,86400.);
+
+    h_livetime->GetXaxis()->SetTitle("Date");
+    h_livetime->GetYaxis()->SetTitle("Livetime [days]");
+    h_livetime->GetXaxis()->SetTimeDisplay(1);
+    h_livetime->GetXaxis()->SetTimeFormat("%b");
+
+    time_start->GetXaxis()->SetTitle("Date");
+    time_start->GetYaxis()->SetTitle("Livetime [days]");
+    time_start->GetXaxis()->SetTimeDisplay(1);
+    time_start->GetXaxis()->SetTimeFormat("%b");
+
+    time_end->GetXaxis()->SetTitle("Date");
+    time_end->GetYaxis()->SetTitle("Livetime [days]");
+    time_end->GetXaxis()->SetTimeDisplay(1);
+    time_end->GetXaxis()->SetTimeFormat("%b");
+
+    rate_bruno->GetXaxis()->SetTitle("Date");
+    rate_bruno->GetYaxis()->SetTitle("Event Rate / day [day^{-1}]");
+    rate_bruno->GetXaxis()->SetTimeDisplay(1);
+    rate_bruno->GetXaxis()->SetTimeFormat("%b");
+  
+    rate_48kg->GetXaxis()->SetTitle("Date");
+    rate_48kg->GetYaxis()->SetTitle("Event Rate / day [day^{-1}]");
+    rate_48kg->GetXaxis()->SetTimeDisplay(1);
+    rate_48kg->GetXaxis()->SetTimeFormat("%b");
+  
+    rate_SR->GetXaxis()->SetTitle("Date");
+    rate_SR->GetYaxis()->SetTitle("Event Rate / day [day^{-1}]");
+    rate_SR->GetXaxis()->SetTimeDisplay(1);
+    rate_SR->GetXaxis()->SetTimeFormat("%b");
+  //---------------------------------------------//
+
+  TH1F *rate_all = new TH1F("rate_all","rate",1000,-0.01,19.99); 
+  TH1F *event_reduced = new TH1F("event_reduced","",1000,-5.005,4.995); 
+
+
+//---------------------------------------------------------------------------------//
+
+
+//------------------LOOP-------------------------------------------------------//   
    Long64_t nentries = fChain->GetEntriesFast();
+   
    bool isFirst = true;
    int start = 0;
-   int last = 0;
+   int end = 0;
 
    int counter = 0;
 
@@ -61,20 +111,22 @@ void rate::Loop()
 
 	fillBranches(ientry); // fill branch!
 
+	if(isFirst) { start = XeT1.TimeSec ;  isFirst=false;} // time of first measure s
+
 	cut_flow->Fill("Tot",1.);
 	
 	if(!run_12_bruno()) continue;
 		cut_flow->Fill("bruno_basic",1.);
+		event_time_bruno->Fill(XeT1.TimeSec - start);
 
-//	if(!X48kg0()) continue;
+	if(!X48kg0()) continue;
 		cut_flow->Fill("X48kg0",1.);
+		event_time_48kg->Fill(XeT1.TimeSec - start );
 
-//	if( XeT3.cxS1sTot->at(0) >= 20. || XeT3.cxS1sTot->at(0) <= 3. ) continue;
+	if( XeT3.cxS1sTot->at(0) >= 20. || XeT3.cxS1sTot->at(0) <= 3. ) continue;
 		cut_flow->Fill("S1Tot_3_20",1.);
+		event_time_SR->Fill(XeT1.TimeSec - start );
 
-	if(!Xs2peakpos0()) continue;
-		cut_flow->Fill("Xs2peakpos0",1.);
-		counter++;
 	for(int i=1; i < rate_r->GetNbinsX(); i++)
 		{
 		if(R() < rate_r->GetBinLowEdge(i) ) rate_r->Fill(rate_r->GetBinCenter(i));
@@ -84,105 +136,87 @@ void rate::Loop()
 	rate_2d->Fill(R(), Z());	
    	rate_S1_S2->Fill(XeT3.cxS1sTot->at(0), XeT3.cS2sTot->at(0));
 
+        counter++;
+
 	if(!X34kg2()) continue;
 		cut_flow->Fill("X34kg2",1.);
 	
    }
 
+//--------------------------------------------------------------//
 
- 
+//---------------------FINALISE---------------------------------------------------//
+   
+  end = XeT1.TimeSec;  //time of last measure in s
+
   TString dataset_name_full = XeT1.Filename;
   TString dataset_name(dataset_name_full(0,17));
 
-  TString type("");
-
+  TString type("NONE");
   double livetime = DatasetLivetime(dataset_name, type);
+  cout << "\nDataset Type:  " << type << endl;
   cout << "Corrected Live Time " <<  livetime << " days  =  " << livetime * 24. * 3600. << " sec" << endl;
 
   total_livetime->Fill(0., livetime);
 
   cut_flow->Fill("Livetime",livetime);
 
-  //rate_r->Scale(1./livetime);
-  ///rate_z->Scale(1./livetime);
-
+  cout << "\n--------------------------------------\n-----------------CUTFLOW--------------\n--------------------------------------" << endl;
   for(int i=1; i < 7; i++) 
 	cout << cut_flow->GetXaxis()->GetBinLabel(i) << "   " << cut_flow->GetBinContent(i) << "  --> Rate = " << 
 		cut_flow->GetBinContent(i) / livetime << endl;
 
-//  cut_flow->Scale(1./livetime);
 
-  TH1F *rate_bruno = new TH1F("rate_bruno","rate VS dataset time",80000,1200000000.,1500000000.); // unixtime from 208 to 2017 in bin o ~1h
-  TH1F *rate_peakpos = new TH1F("rate_peakpos","rate VS dataset time",80000,1200000000.,1500000000.); // unixtime from 208 to 2017 in bin o ~1h
-  TH1F *rate_34kg = new TH1F("rate_34kg","rate VS dataset time",80000,1200000000.,1500000000.); // unixtime from 208 to 2017 in bin o ~1h
+  time_start->Fill(start, 1.);
+  time_end->Fill(end, 1.);
+  h_livetime->Fill( XeT1.TimeSec, livetime);
 
-  TH1F *rate_all = new TH1F("rate_all","rate",1000,-0.01,19.99); // unixtime from 208 to 2017 in bin o ~1h
-  TH1F *event_reduced = new TH1F("event_reduced","",1000,-5.005,4.995); // unixtime from 208 to 2017 in bin o ~1h
+  rate_bruno->Fill( XeT1.TimeSec, cut_flow->GetBinContent(2) / livetime);
+  rate_bruno->SetBinError(rate_bruno->FindBin( XeT1.TimeSec), cut_flow->GetBinError(2) /livetime);
 
-  rate_bruno->GetXaxis()->SetTitle("Date");
-  rate_bruno->GetYaxis()->SetTitle("Event Rate / day [day^{-1}]");
-  rate_bruno->GetXaxis()->SetTimeDisplay(1);
-  rate_bruno->GetXaxis()->SetTimeFormat("%b");
-  
-  rate_peakpos->GetXaxis()->SetTitle("Date");
-  rate_peakpos->GetYaxis()->SetTitle("Event Rate / day [day^{-1}]");
-  rate_peakpos->GetXaxis()->SetTimeDisplay(1);
-  rate_peakpos->GetXaxis()->SetTimeFormat("%b");
-  
-  rate_34kg->GetXaxis()->SetTitle("Date");
-  rate_34kg->GetYaxis()->SetTitle("Event Rate / day [day^{-1}]");
-  rate_34kg->GetXaxis()->SetTimeDisplay(1);
-  rate_34kg->GetXaxis()->SetTimeFormat("%b");
-  
-  rate_bruno->Fill( XeT1.TimeSec, cut_flow->GetBinContent(4) / livetime);
-  rate_bruno->SetBinError(rate_bruno->FindBin( XeT1.TimeSec), cut_flow->GetBinError(4) /livetime);
+  rate_48kg->Fill( XeT1.TimeSec, cut_flow->GetBinContent(3) / livetime);
+  rate_48kg->SetBinError(rate_48kg->FindBin( XeT1.TimeSec), cut_flow->GetBinError(3) /livetime);
 
-  rate_peakpos->Fill( XeT1.TimeSec, cut_flow->GetBinContent(5) / livetime);
-  rate_peakpos->SetBinError(rate_peakpos->FindBin( XeT1.TimeSec), cut_flow->GetBinError(5) /livetime);
-
-  rate_34kg->Fill( XeT1.TimeSec, cut_flow->GetBinContent(6) / livetime);
-  rate_34kg->SetBinError(rate_34kg->FindBin( XeT1.TimeSec), cut_flow->GetBinError(6) /livetime);
+  rate_SR->Fill( XeT1.TimeSec, cut_flow->GetBinContent(4) / livetime);
+  rate_SR->SetBinError(rate_SR->FindBin( XeT1.TimeSec), cut_flow->GetBinError(4) /livetime);
 
   event_reduced->Fill( ((float)counter - 2.409 * livetime )/sqrt(2.409*livetime));
 
-  TH1F *h_livetime = new TH1F("h_livetime","livetime VS dataset time",80000,1200000000.,1500000000.); // unixtime from 208 to 2017 in bin o ~1h
-  TH1F *h_test = new TH1F("h_test","livetime VS dataset time",80000,1200000000.,1500000000.); // unixtime from 208 to 2017 in bin o ~1h
-
-  h_livetime->GetXaxis()->SetTitle("Date");
-  h_livetime->GetYaxis()->SetTitle("Livetime [days]");
-  h_livetime->GetXaxis()->SetTimeDisplay(1);
-  h_livetime->GetXaxis()->SetTimeFormat("%b");
-  h_livetime->Fill( XeT1.TimeSec, livetime);
-  h_test->Fill( XeT1.TimeSec, 1.);
-
   rate_all->Fill( (float)counter / livetime );
 
-  TFile *file = new TFile(outDir + "cutFlow_" + filename ,"RECREATE");
+
+  TFile *file = new TFile(outDir + "rate_" + filename ,"RECREATE");
   file->cd();
 
-  cut_flow->Write(dataset_name); // if same dataset has more files will be hadd correctly
-  rate_bruno->Write();
-  rate_peakpos->Write();
-  rate_34kg->Write();
-  h_livetime->Write();
-  total_livetime->Write();
-  rate_z->Write();
-  rate_r->Write();
-  rate_all->Write();
-  event_reduced->Write();
-  h_test->Write();
-  rate_2d->Write("rate_2d_"+dataset_name);
-  rate_S1_S2->Write("rate_SS_"+dataset_name);
+  cut_flow->Write(dataset_name+"_"+type); // if same dataset has more files will be hadd correctly
+  rate_bruno->Write("rate_bruno_"+type);
+  rate_48kg->Write("rate_48kg_"+type);
+  rate_SR->Write("rate_SR_"+type);
+  event_time_bruno->Write("event_time_bruno_"+type);
+  event_time_48kg->Write("event_time_48kg_"+type);
+  event_time_SR->Write("event_time_SR_"+type);
+  h_livetime->Write("livetime_"+type);
+  total_livetime->Write("total_livetime_"+type);
+  rate_z->Write("rate_z_"+type);
+  rate_r->Write("rate_r_"+type);
+  rate_all->Write("rate_all_"+type);
+  event_reduced->Write("event_reduced_"+type);
+  time_start->Write("time_start_"+type);
+  time_end->Write("time_end_"+type);
+  rate_2d->Write("rate_2d_"+dataset_name+"_"+type);
+  rate_S1_S2->Write("rate_SS_"+dataset_name+"_"+type);
 
   file->Close();
   delete file;
+
+//-----------------------------------------//
 }
 
   bool  rate::		run_12_bruno()
 	{
 	return ( Xsignalnoise5() && Xs1width0() && Xentropy1() && Xs2peaks2() && Xs1coin2() && Xhighlog1()
 		&& Xs2single3() && Xs1single5() && Xveto2()  && Xs2width11() && XPL013_97() && Xposrec1() 
-		&& Xs2chisquare1() && Xlownoise0_m() ) ;
+		&& Xs2chisquare1() && Xlownoise0_m() && Xs2peakpos0()) ;
 	}
 
 #endif
